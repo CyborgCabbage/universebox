c49e1b24a3cf81f034846cf8dc0e8cc3a1b877b1
CyborgCabbage <finlaylaidlaw9@gmail.com>
2021-12-26 12:37:40 +0000

    - UniverseBoxBlock now drops on break in creative mode

--- src/main/java/cyborgcabbage/universebox/block/UniverseBoxBlock.java
+++ src/main/java/cyborgcabbage/universebox/block/UniverseBoxBlock.java
@@ -1,19 +1,22 @@
 package cyborgcabbage.universebox.block;
 
+import com.sun.jna.platform.win32.Winnetwk;
 import cyborgcabbage.universebox.UniverseBox;
 import cyborgcabbage.universebox.block.entity.UniverseBoxBlockEntity;
 import cyborgcabbage.universebox.pocket.PocketState;
 import cyborgcabbage.universebox.portal.DependentPortal;
-import net.minecraft.block.Block;
-import net.minecraft.block.BlockEntityProvider;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.ShapeContext;
+import net.minecraft.block.*;
 import net.minecraft.block.entity.BlockEntity;
+import net.minecraft.block.entity.ShulkerBoxBlockEntity;
 import net.minecraft.entity.Entity;
+import net.minecraft.entity.ItemEntity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.loot.context.LootContext;
+import net.minecraft.loot.context.LootContextParameters;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.function.BooleanBiFunction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -24,9 +27,12 @@ import net.minecraft.world.BlockView;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.UUID;
 
 public class UniverseBoxBlock extends Block implements BlockEntityProvider {
+    public static final Identifier POCKET_INDEX = new Identifier("pocketIndex");
+
     private static final VoxelShape BLOCK_SHAPE = VoxelShapes.combineAndSimplify(VoxelShapes.fullCube(), Block.createCuboidShape(1.0, 1.0, 1.0, 15.0, 16.0, 15.0), BooleanBiFunction.ONLY_FIRST);
     public UniverseBoxBlock(Settings settings) {
         super(settings);
@@ -99,6 +105,21 @@ public class UniverseBoxBlock extends Block implements BlockEntityProvider {
             innerPortal.setup(pos, outerDimension, blockEntity.pocketIndex);
             innerPortal.world.spawnEntity(innerPortal);
         }
+    }
+
+    @Override
+    public void onBreak(World world, BlockPos pos, BlockState state, PlayerEntity player) {
+        BlockEntity blockEntity = world.getBlockEntity(pos);
+        if (blockEntity instanceof UniverseBoxBlockEntity universeBoxBlockEntity) {
+            if (!world.isClient && player.isCreative() && universeBoxBlockEntity.pocketIndex != -1) {
+                ItemStack itemStack = new ItemStack(UniverseBox.UNIVERSE_BOX_BLOCK);
+                blockEntity.setStackNbt(itemStack);
+                ItemEntity itemEntity = new ItemEntity(world, (double) pos.getX() + 0.5, (double) pos.getY() + 0.5, (double) pos.getZ() + 0.5, itemStack);
+                itemEntity.setToDefaultPickupDelay();
+                world.spawnEntity(itemEntity);
+            }
+        }
+        super.onBreak(world, pos, state, player);
     }
 
     @Override
